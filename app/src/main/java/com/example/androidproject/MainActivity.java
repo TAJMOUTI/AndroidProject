571d1c4cb85400dc73195d1a5a8fa90a7165f344
Jacques <jack.polart@gmail.com>
2020-10-07 13:09:54 +0200

    create landing page

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_to_landing_page = findViewById(R.id.btn_to_landing_page);
+        btn_to_landing_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Landing_page.class);
+                startActivity(intent);
+            }
+        });
     }
 }
