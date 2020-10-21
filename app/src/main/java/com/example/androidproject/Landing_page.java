ba4aa0128e25c92e5e887d27e1bdc175c416e8a2
Jacques <jack.polart@gmail.com>
2020-10-21 11:37:21 +0200

    Add sign up page

--- app/src/main/java/com/example/androidproject/Landing_page.java
+++ app/src/main/java/com/example/androidproject/Landing_page.java
@@ -2,7 +2,10 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Landing_page extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Landing_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        Button btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity_sign_in_page.class);
+                startActivity(intent);
+            }
+        });
     }
 }
