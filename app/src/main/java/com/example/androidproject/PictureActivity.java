067da04bc83b90ecb9d6f1cd1571eee40704e54a
Jacques <jack.polart@gmail.com>
2021-01-07 18:14:30 +0100

    API now work with photo taken by the phone

--- app/src/main/java/com/example/androidproject/PictureActivity.java
+++ app/src/main/java/com/example/androidproject/PictureActivity.java
@@ -1,24 +1,21 @@
 package com.example.androidproject;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
-import android.widget.AdapterView;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,145 +27,129 @@ import clarifai2.dto.model.Model;
 import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Concept;
 
+
 public class PictureActivity extends AppCompatActivity {
 
+    /**
+     * We need a class that extends AsyncTask to run the API
+     * call : new ClarifaiTask().execute(); to analyse the photo stored in variable : bitmap
+     * .execute(); on a ClarifaiTask object will call the function : doInBackground()
+     */
+    private class ClarifaiTask extends AsyncTask {
 
-    String api_key = "f50c24a977bb49318a2f3ad3b2440b22";
+        @Override
+        protected ArrayList<String> doInBackground(Object[] objects) {
+            if (bitmap != null) {
+                // the list where we store the ingredients
+                ArrayList<String> labels = new ArrayList<String>();
+
+                // found the ingredients with the API
+                final ClarifaiClient client = new ClarifaiBuilder(api_key).buildSync();
+                Model<Concept> generalModel = client.getDefaultModels().foodModel();
+                PredictRequest<Concept> request = generalModel.predict().withInputs(
+                        ClarifaiInput.forImage(bitmapToBytes(bitmap))
+                );
+                List<ClarifaiOutput<Concept>> result = request.executeSync().get();
+
+                // check the result
+                if (!result.isEmpty()) {
+                    // store the data in our list
+                    for(int i = 0; i < result.size(); i++){
+                        for(int j = 0; j < result.get(i).data().size(); j++){
+                            String labelResult = result.get(i).data().get(j).name();
+                            labels.add(labelResult);
+                        }
+                    }
+
+                    // return the labels
+                    return labels;
+                } else {
+                    Toast.makeText(getApplicationContext(), "nothing to see...", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.d("ANALYSE_PHOTO", "variable bitmap is null");
+            }
+            return null;
+        }
+
+        /**
+         * this functioin is executed after doInBackground()
+         * @param o : it contain all the ingredients names
+         */
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+
+            // we display the ingredients on the screen
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(PictureActivity.this,android.R.layout.simple_list_item_1, (ArrayList<String>) o);
+            listIngredients.setAdapter(arrayAdapter);
+        }
+    }
+
+
+    private String api_key = "f50c24a977bb49318a2f3ad3b2440b22";
     private static final int REQUEST_TAKE_PHOTO = 1;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private String photoPath = null;
-    Bitmap bitmap;
-    ImageView photoPlate;
-    ListView listIngredients;
-    ArrayList<String> ingredients;
-    Button nextButton;
-
-
-    // Ce qui va appeler l'API pour recuperer les ingredients et les placer dans une liste
-    private class ClarifaiTask extends AsyncTask<File, Void, ArrayList<String>> {
-        @Override
-        protected ArrayList<String> doInBackground(File... images) {
-
-            //In case we would like remove doubles :
-            //HashSet<String> Labels = new HashSet<>();
-            ArrayList<String> Labels = new ArrayList<String>();//Creation du tableau pour ranger les ingredients analysés par l'API
-
-            ClarifaiClient client = new ClarifaiBuilder(api_key).buildSync();// Lancement de l'API
-            List<ClarifaiOutput<Concept>> predictionResults;
-
-            //Make prediction for each image in parameters
-            for (File image : images) {
-                bitmap = BitmapFactory.decodeFile(image.getPath()); //decode le chemin d'un fichier et cree un fichier bitmap
-                Model<Concept> generalModel = client.getDefaultModels().foodModel(); //utilise le modèle foodModel de Clarifai
-                PredictRequest<Concept> request = generalModel.predict().withInputs(
-                        ClarifaiInput.forImage("https://samples.clarifai.com/food.jpg")//Lien vers une image comme exemple
-                );
-                List<ClarifaiOutput<Concept>> result = request.executeSync().get(); // Ajoute les résultats dans la liste result
-
-                //On récupère les labels de chaque objet et on les place dans un tableau
-                for(int i = 0; i < result.size(); i++){
-                    for(int j = 0; j < result.get(i).data().size(); j++){
-                        String labelResult = result.get(i).data().get(j).name();
-                        Labels.add(labelResult);
-                    }
-                }
-                //In case we would like to remove doubles :
-                // ArrayLabels = new ArrayList<>(Labels);
-            }
-            System.out.println(Labels);
-            return Labels;
-        }
-
-        protected void onPostExecute(final ArrayList<String> ObjectLabels) {
-            listIngredients = findViewById(R.id.ingredients);
-            photoPlate = findViewById(R.id.photoPlate);
-            nextButton = findViewById(R.id.nextButton);
-
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(PictureActivity.this,android.R.layout.simple_list_item_1, ObjectLabels);
-            listIngredients.setAdapter(arrayAdapter);
-
-            listIngredients.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    // TODO if item is already in the list
-                    ingredients.add(ObjectLabels.get(position));
-                    view.setBackgroundColor(0x805496BC); //TODO changer la methode de changement de couleur
-                    view.setEnabled(false);
-
-                    for(int i = 0; i < ingredients.size(); i++) {
-                        System.out.println(ingredients.get(i));
-                    }
-
-                    if(ingredients.isEmpty()){
-                        nextButton.setEnabled(false);
-                        nextButton.setBackgroundColor(0x805496BC); //TODO changer la methode de changement de couleur
-                    }
-                }
-            });
-
-            //Affichage de la photo crée
-            photoPlate.setImageBitmap(bitmap);
-
-            //Redirection vers la page d'ajout des détails sur les quantités
-            //Passage de la liste ingredients en paramètre
-            nextButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(PictureActivity.this, PictureActivityDetails.class);
-                    intent.putExtra("INGREDIENTS_LIST", ingredients);
-                    startActivity(intent);
-                }
-            });
-        }
-    }
+    private Bitmap bitmap;
+    private ImageView photoPlate;
+    private ListView listIngredients;
+    private ArrayList<String> ingredients;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//Quand la page se crée la fonction takePicture est directement crée
-        takePicture();
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
-        ingredients = new ArrayList<>();
+
+        // set variables
+        listIngredients = findViewById(R.id.ingredients);
+        photoPlate = findViewById(R.id.photoPlate);
+        nextButton = findViewById(R.id.nextButton);
+
+        openCamera();
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            // start the camera activity
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // when the photo will be taken, the program will go in : onActivityResult()
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        //Si la photo a été prise on l'envoie à Clarifai pour vérifier
-        if (photoPath != null) {
-            new ClarifaiTask().execute(new File(photoPath));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // get extra
+            Bundle extras = data.getExtras();
+            // save photo
+            bitmap = (Bitmap) extras.get("data");
+            // display photo
+            photoPlate.setImageBitmap(bitmap);
+            // analyse the photo
+            new ClarifaiTask().execute();
+        } else {
+            Intent intent = new Intent(getApplicationContext(), Main_Page.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
         }
     }
-// La fonction pour prendre des photos
-    public void takePicture() {
-        //Ouvrir l'appareil photo
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //System.out.println(takePictureIntent.resolveActivity(getPackageManager()));
-        //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 
-        //if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-         File photoFile;
-            try {
-                //Essayer d'enregistrer la photo
-                File storageDir = getFilesDir();//Recupère le dossier ou l'on veut enregistrer la photo
-                photoFile = File.createTempFile(
-                        "SNAPSHOT",
-                        ".jpg",
-                        storageDir
-                );
-
-                photoPath = photoFile.getAbsolutePath();//Recuperation de la route vers la photo prise
-            } catch (IOException ex) {
-                return;
-            }
-
-            Uri photoURI = FileProvider.getUriForFile(this,
-                    "com.example.androidproject.fileprovider",
-                    photoFile);
-
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        //}
+    /**
+     * convert a Bitmap to a Byte array
+     * @param myPhoto : the bitmap picture we want to convert
+     * @return : a byte array of the bitmap picture we sent
+     */
+    public byte[] bitmapToBytes(Bitmap myPhoto) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        myPhoto.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+        byte[] myBytes = bos.toByteArray();
+        return myBytes;
     }
-
 }
